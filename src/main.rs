5c90c99936a31c554574b71951d88de38579c2a1
Sakaki Aruka <99568054+Sakaki-Aruka@users.noreply.github.com>
2024-05-11 21:34:57 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::path::{Path, PathBuf};
+use std::env;
+use colored::Colorize;
+
 fn main() {
     println!("Hello, world!");
 }
+
+fn exist_config_file() -> Option<bool> {
+    // checks toml format and contents.
+    let path = env::current_dir();
+    if path.is_err() {
+        println!("{}", "failed to get current directory. try again after sometime.".red());
+        return None;
+    }
+    let mut path: PathBuf = env::current_dir().unwrap();
+
+
+    Some(false)
+}
+
